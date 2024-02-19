0604ff648365d685b433a70e9725d816b4a32ad0
Mistake <hfmistake@protonmail.com>
2024-02-19 02:11:21 -0300

    feat(main): OCR Reader

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from ultralytics import YOLO
 from predictions import Prediction
 
+
 if __name__ == '__main__':
-    my_data = {
-        "video_source": "videos/2.mp4",
+    vehicle_indexes = [2, 3, 5, 7]
+    plate_index = [0]
+    plate = {
+        "video_source": "videos/5.mp4",
         "line": [100, 300, 1800, 300],
-        "object_indices": [2, 3, 5, 7],
+        "object_indices": plate_index,
         "directory": "results/results22"
     }
-    my_model = YOLO('models/yolov8n.pt')
-    prediction = Prediction(my_model, my_data)
-    prediction.simple_track_and_capture()
+    normal = {
+        "video_source": "videos/5.mp4",
+        "line": [100, 300, 1800, 300],
+        "object_indices": vehicle_indexes,
+        "directory": "results/results22"
+    }
+    pre_trained = YOLO('models/yolov8n.pt')
+    plate_model = YOLO('models/treino5.pt')
+    normal_prediction = Prediction(pre_trained, normal)
+    plate_prediction = Prediction(plate_model, plate)
+    normal_prediction.visualize()
--- predictions.py
+++ predictions.py
@@ -1,19 +1,41 @@
 import cv2
+import numpy as np
 import torch
 import uuid as uid
 from PIL import Image
+import pytesseract
+
+
+def pre_process_plate_image(plate_image):
+    plate_image = cv2.resize(plate_image, (0, 0), fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
+    plate_image = cv2.cvtColor(plate_image, cv2.COLOR_BGR2GRAY)
+    plate_image = cv2.threshold(plate_image, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    plate_image = cv2.medianBlur(plate_image, 3)
+    plate_image = cv2.GaussianBlur(plate_image, (3, 3), 0)
+    return plate_image
+
+
+def extract_text_from_plate(plate_image, pre_processing=False):
+    config = "--psm 6 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    plate_image = np.array(plate_image)
+    if pre_processing:
+        plate_image = pre_process_plate_image(plate_image)
+    cv2.imshow("Plate", plate_image)
+    result = pytesseract.image_to_string(plate_image, config=config)
+    result = result.replace("\n", "")
+    if result:
+        return result
 
 
 class Prediction:
     def __init__(self, model, data: dict):
         self.model = model
         self.data = data
-        self.captured_ids = set()
-        Prediction.check_gpu()
 
     @staticmethod
     def check_gpu():
         if torch.cuda.is_available():
+            torch.cuda.empty_cache()
             gpu_name = torch.cuda.get_device_name(torch.cuda.current_device())
             print(f"Starting with GPU: {gpu_name}")
         else:
@@ -35,7 +57,7 @@ class Prediction:
                  (88, 85, 232), 4)
 
     def _process_frame(self, frame):
-        results = self.model.track(frame, classes=self.data["object_indices"], conf=0.6, iou=0.5)
+        results = self.model.track(frame, classes=self.data["object_indices"], conf=0.6, iou=0.5, persist=True)
         return self.draw_visualization_elements(results[0], results[0].plot())
 
     def check_and_draw_collision(self, result, original_frame):
@@ -47,12 +69,14 @@ class Prediction:
             center_point = (center_x, center_y)
             cv2.circle(original_frame, center_point, 7, (88, 85, 232), -1)
             if box.id is not None and self.is_point_inside_line(center_x, center_y, int(box.id.item())):
-                print("teste")
-                cv2.putText(original_frame, "Captura realizada", (0, 200),
-                            cv2.FONT_HERSHEY_SIMPLEX, 2, (88, 85, 232), 3, cv2.LINE_AA)
-                track_id = int(box.id.item())
-                msg = f"Captured vehicle id: {track_id}"
-                cv2.imshow(msg, original_frame)
+                plate_image = Image.fromarray(result.orig_img[y1:y2, x1:x2])
+                plate_text = extract_text_from_plate(plate_image, pre_processing=True)
+                if plate_text:
+                    print(f"Placa: {plate_text}")
+                cv2.rectangle(original_frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+                cv2.putText(original_frame, f"Placa: {plate_text}", (0, 200), cv2.FONT_HERSHEY_SIMPLEX, 0.9,
+                            (0, 0, 255), 2)
+                cv2.imshow("Captured Plate", original_frame)
 
         return original_frame
 
@@ -62,24 +86,33 @@ class Prediction:
         return original_frame
 
     def visualize(self):
+        Prediction.check_gpu()
         cap = cv2.VideoCapture(self.data["video_source"])
+        paused = False
+
         while cap.isOpened():
-            success, frame = cap.read()
-            if not success:
+            if not paused:
+                success, frame = cap.read()
+                if not success:
+                    break
+                annotated_frame = self._process_frame(frame)
+                cv2.imshow("YOLOv8 Inference", annotated_frame)
+
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord("q"):
                 break
-            annotated_frame = self._process_frame(frame)
-            cv2.imshow("YOLOv8 Inference", annotated_frame)
-            if cv2.waitKey(1) & 0xFF == ord("q"):
-                break
+            elif key == ord("p"):
+                paused = not paused
 
         cap.release()
         cv2.destroyAllWindows()
         cv2.waitKey(1)
 
     def simple_track_and_capture(self):
+        Prediction.check_gpu()
         directory = self.data["directory"]
         object_indices = self.data["object_indices"]
-        results = self.model.track(self.data["video_source"], stream=True, iou=0.5, classes=object_indices, conf=0.6)
+        results = self.model.track(self.data["video_source"], stream=True, iou=0.5, classes=object_indices, conf=0.5)
         for result in results:
             boxes = result.boxes
             for box in boxes:
@@ -88,5 +121,9 @@ class Prediction:
                 center_x, center_y = x1 + width // 2, y1 + height // 2
                 if box.id is not None and self.is_point_inside_line(center_x, center_y, int(box.id.item())):
                     im = Image.fromarray(result.orig_img)
+                    plate_image = Image.fromarray(result.orig_img[y1:y2, x1:x2])
+                    plate_text = extract_text_from_plate(plate_image)
+                    if plate_text:
+                        print(f"Placa: {plate_text}")
                     im.save(f'{directory}/{uid.uuid4()}.jpg')
                     print("captura realizada")
